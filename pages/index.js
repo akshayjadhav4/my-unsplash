a0e98ba72974da3f1b41bf717d3161b4c40c2f4f
AKSHAY <akshayjadhav461999@gmail.com>
2021-01-11 17:37:58 +0530

    home page layout created

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 import Head from "next/head";
+import Header from "../components/Header/Header";
 
 export default function Home() {
   return (
-    <div>
+    <div className="container mx-auto px-4 flex flex-col min-h-screen">
       <Head>
         <title>My Unsplash</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Set up done</h1>
+      <Header />
+      <h1>Content</h1>
+
+      <h1 className="mt-auto text-center font-semibold text-gray-400">
+        {`<Akshay Jadhav(AKS) @devchallenges.io/>`}
+      </h1>
     </div>
   );
 }
