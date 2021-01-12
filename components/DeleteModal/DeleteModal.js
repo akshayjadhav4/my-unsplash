17e4415a7bd48c00579ab69fed1322c82df3d3cc
AKSHAY <akshayjadhav461999@gmail.com>
2021-01-12 15:40:19 +0530

    hiding button for unauthorized user

--- components/DeleteModal/DeleteModal.js
+++ components/DeleteModal/DeleteModal.js
@@ -71,15 +71,16 @@ const DeleteModal = ({ authorId, photoId }) => {
                       >
                         Cancle
                       </button>
-                      <button
-                        className="bg-red-500 text-white active:bg-green-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-                        type="submit"
-                        style={{ transition: "all .15s ease" }}
-                        onClick={deletePhoto}
-                        disabled={authorId !== user?.uid}
-                      >
-                        Delete
-                      </button>
+                      {authorId === user?.uid && (
+                        <button
+                          className="bg-red-500 text-white active:bg-green-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+                          type="submit"
+                          style={{ transition: "all .15s ease" }}
+                          onClick={deletePhoto}
+                        >
+                          Delete
+                        </button>
+                      )}
                     </div>
                   ) : (
                     <>
